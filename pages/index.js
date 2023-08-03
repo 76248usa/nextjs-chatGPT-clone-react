796a3a10ab4073a5bb7baeef4b50261f3a54fe8e
Elsabe <76248usa@gmail.com>
2023-08-03 18:47:39 -0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,49 @@
 import Head from "next/head";
+import Link from 'next/link';
+import {useUser} from '@auth0/nextjs-auth0/client'
+import { getSession } from "@auth0/nextjs-auth0";
 
 export default function Home() {
+  const {isLoading,error,user} = useUser();
+
+  if(isLoading) return <div>Loading...</div>
+  if(error) return <div>{error.message}</div>
+
   return (
-    <div>
+    <>
       <Head>
-        <title>Next JS ChatGPT Starter</title>
+        <title>Chatty Elsabe - Login or Signup</title>
       </Head>
-      <h1>Welcome to the Next JS &amp; ChatGPT Starter</h1>
-    </div>
+      
+      <div className="min-h-screen w-full bg-gray-800 text-white text-center
+      flex justify-center items-center">
+        <div>
+        {user && <Link href="/api/auth/logout" className="rounded-md
+        bg-emerald-500 px-4 py-2 text-white
+        hover:bg-emerald-600">Logout</Link>}
+        {!user && <>
+        <Link href="/api/auth/login" 
+         className="btn"> Login</Link>
+        <Link href="/api/auth/signup"
+         className="btn"> Signup</Link>
+        </>  }
+        </div>          
+      </div>
+    </>
   );
 }
+
+export const getServerSideProps = async (ctx) => {
+  const session = await getSession(ctx.req, ctx.res)
+  if(session){
+    return {
+      redirect: {
+        destination: '/chat'
+      }
+    }
+  }
+  return {
+    props: {}
+  }
+}
+
